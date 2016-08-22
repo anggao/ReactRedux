2c9acfb75a880e902d1348d7b4d5c3b80be2ba2e
Ang Gao <ang.gao87@gmail.com>
2016-08-23 00:31:29 +0100

    add login page

--- client/routes.js
+++ client/routes.js
@@ -4,10 +4,12 @@ import App from './components/App'
 
 import Greetings from './components/Greetings'
 import SignupPage from './components/signup/SignupPage'
+import LoginPage from './components/login/LoginPage'
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={Greetings} />
     <Route path="signup" components={SignupPage} />
+    <Route path="login" components={LoginPage} />
   </Route>
 )
