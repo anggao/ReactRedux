1ed4c53c55bb040b1084272013042ba7e42b7350
Ang Gao <ang.gao87@gmail.com>
2016-08-23 13:59:48 +0100

    add server side authentication

--- client/routes.js
+++ client/routes.js
@@ -5,11 +5,13 @@ import App from './components/App'
 import Greetings from './components/Greetings'
 import SignupPage from './components/signup/SignupPage'
 import LoginPage from './components/login/LoginPage'
+import NewEventPage from './components/events/NewEventPage'
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={Greetings} />
     <Route path="signup" components={SignupPage} />
     <Route path="login" components={LoginPage} />
+    <Route path="new-event" components={NewEventPage} />
   </Route>
 )
